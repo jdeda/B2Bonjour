8dc6908ffae0f8e63709e860e2b4e41553853bb3
jdeda <jldeda00@gmail.com>
2023-07-10 07:46:24 -0400

    Initial Commit

---
+++ BackBlazeDemo/BackBlazeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackBlazeDemoApp.swift
+//  BackBlazeDemo
+//
+//  Created by Jesse Deda on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BackBlazeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
