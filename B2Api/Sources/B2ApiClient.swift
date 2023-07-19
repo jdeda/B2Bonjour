d59a840bb27ccdfe8f2ecfe3f8cc135c445e716f
Klajd Deda <kdeda@id-design.com>
2023-07-19 09:40:20 -0400

    * Fixes to Log4swift. * Fixes to the tests which all should pass except testListComputers which uses the b1 API

--- B2Api/Sources/B2ApiClient.swift
+++ B2Api/Sources/B2ApiClient.swift
@@ -26,22 +26,22 @@ import XCTestDynamicOverlay
 /// Repeat these for each endpoint you care
 ///
 public struct B2ApiClient {
-  /// This method is a re-write of the B2Api.listBuckets() but it uses structured concurrency and the client
-  /// architecture from TCA. It will allow us to write swiftUI previews and test cases much faster.
-  ///
-  /// What we want here is to make this funcs async. Since we are inside an async context
-  /// we do not have to burden ourselves with blocks.
-  /// What this does is simplify the implementation a lot
-  ///
-  /// - Parameters:
-  ///     - none: We depend on values captured by self
-  /// - Returns: [Bucket] an array of Bucket instances
-  /// - Throws: B2Error if anything goes wrong
-  ///
-  public var listBuckets: @Sendable (_ parameter: ListBuckets) async throws -> [ListBuckets.Response.Bucket]
-  public var listEntriesInDir: @Sendable (_ parameter: ListEntriesInDir) async throws -> [ListEntriesInDir.Response.File]
-  public var listComputers: @Sendable (_ parameter: ListComputers) async throws -> [ListComputers.Response.Computer]
-
+    /// This method is a re-write of the B2Api.listBuckets() but it uses structured concurrency and the client
+    /// architecture from TCA. It will allow us to write swiftUI previews and test cases much faster.
+    ///
+    /// What we want here is to make this funcs async. Since we are inside an async context
+    /// we do not have to burden ourselves with blocks.
+    /// What this does is simplify the implementation a lot
+    ///
+    /// - Parameters:
+    ///     - none: We depend on values captured by self
+    /// - Returns: [Bucket] an array of Bucket instances
+    /// - Throws: B2Error if anything goes wrong
+    ///
+    public var listBuckets: @Sendable (_ parameter: ListBuckets) async throws -> [ListBuckets.Response.Bucket]
+    public var listEntriesInDir: @Sendable (_ parameter: ListEntriesInDir) async throws -> [ListEntriesInDir.Response.File]
+    public var listComputers: @Sendable (_ parameter: ListComputers) async throws -> [ListComputers.Response.Computer]
+    
     /**
      cdeda@backblaze.com
      July 17, 1023
@@ -50,162 +50,162 @@ public struct B2ApiClient {
      applicationKey: K0025cy1RjM8KO8OmbKPOV2mc9cqQnI
      */
     public var authorizeAccount: @Sendable (_ parameter: AuthorizeAccount.Request) async throws -> AuthorizeAccount.Response
-  
-//  public init(
-//    listBuckets: @escaping (_: ListBuckets) -> [ListBuckets.Response.Bucket],
-//    listEntriesInDir: @escaping (_: ListEntriesInDir) -> [ListEntriesInDir.Response.File],
-//    listComputers: @escaping (_: ListComputers) -> [ListComputers.Response.Computer],
-//    authorizeAccount: @escaping (_: AuthorizeAccount.Request) -> AuthorizeAccount.Response
-//  ) {
-//    self.listBuckets = listBuckets
-//    self.listEntriesInDir = listEntriesInDir
-//    self.listComputers = listComputers
-//    self.authorizeAccount = authorizeAccount
-//  }
-  
+    
+    //  public init(
+    //    listBuckets: @escaping (_: ListBuckets) -> [ListBuckets.Response.Bucket],
+    //    listEntriesInDir: @escaping (_: ListEntriesInDir) -> [ListEntriesInDir.Response.File],
+    //    listComputers: @escaping (_: ListComputers) -> [ListComputers.Response.Computer],
+    //    authorizeAccount: @escaping (_: AuthorizeAccount.Request) -> AuthorizeAccount.Response
+    //  ) {
+    //    self.listBuckets = listBuckets
+    //    self.listEntriesInDir = listEntriesInDir
+    //    self.listComputers = listComputers
+    //    self.authorizeAccount = authorizeAccount
+    //  }
+    
 }
 
 extension DependencyValues {
-  public var b2ApiClient: B2ApiClient {
-    get { self[B2ApiClient.self] }
-    set { self[B2ApiClient.self] = newValue }
-  }
+    public var b2ApiClient: B2ApiClient {
+        get { self[B2ApiClient.self] }
+        set { self[B2ApiClient.self] = newValue }
+    }
 }
 
 extension B2ApiClient: DependencyKey {
-  public static let liveValue = Self(
-    listBuckets: { params in
-      func urlRequest() throws -> URLRequest {
-        let relativeURL = "b2api/v2/b2_list_buckets"
-        let absoluteURL = params.auth.apiUrl.appendingPathComponent(relativeURL)
-        
-        var urlRequest = URLRequest(url: absoluteURL)
-        // in seconds
-        urlRequest.timeoutInterval = 10.0
-        urlRequest.httpMethod = "POST"
-        urlRequest.httpBody = {
-          let request = ListBuckets.Request(accountId: params.auth.accountId)
-          return try? JSONEncoder().encode(request)
-        }()
-        urlRequest.allHTTPHeaderFields = params.auth.authHeaders()
-          .appending(NetworkUtilities.defaultBackblazeHeaders)
-        return urlRequest
-      }
-      
-      do {
-        return try await urlRequest().fetchResponse(ListBuckets.Response.self).buckets
-      } catch {
-        Log4swift[Self.self].error("error: \(error)")
-        throw error
-      }
-    },
-    listEntriesInDir: { params in
-      func urlRequest() throws -> URLRequest {
-        let relativeURL = params.shouldReturnFolders ? "b2api/v2/b2_list_file_versions" : "b2api/v2/b2_list_file_names"
-        let absoluteURL = params.auth.apiUrl.appendingPathComponent(relativeURL)
-        
-        var urlRequest = URLRequest(url: absoluteURL)
-        // in seconds
-        // long timeout for slow loading trees
-        urlRequest.timeoutInterval = 600.0
-        urlRequest.httpMethod = "POST"
-        urlRequest.httpBody = {
-          let delimiter = params.shouldReturnFolders ? "/" : nil
-          let request = ListEntriesInDir.Request(bucketId: params.bucketId, startFileName: params.startFileName, delimiter: delimiter, prefix: params.startFileName)
-          return try? JSONEncoder().encode(request)
-        }()
-        urlRequest.allHTTPHeaderFields = params.auth.authHeaders()
-          .appending(NetworkUtilities.defaultBackblazeHeaders)
-        return urlRequest
-      }
-      
-      do {
-        return try await urlRequest().fetchResponse(ListEntriesInDir.Response.self).files
-      } catch {
-        Log4swift[Self.self].error("error: \(error)")
-        throw error
-      }
-    },
-    listComputers: { params in
-      func urlRequest() throws -> URLRequest {
-        let relativeURL = "api2/list_b1_computers"
-        let absoluteURL = params.auth.apiUrl.appendingPathComponent(relativeURL)
-          .appendingQueryItem(name: "user_id", value: params.auth.accountId)
-          .appendingQueryItem(name: "bz_device_id", value: params.deviceId)
-          .appendingQueryItem(name: "cluster_num", value: params.clusterNum)
-        
-        // Some POST requests may also want parameters in the url
-        // and can have a nil body
-        
-        var urlRequest = URLRequest(url: absoluteURL)
-        // in seconds
-        urlRequest.timeoutInterval = 10.0
-        urlRequest.httpMethod = "POST"
-        urlRequest.httpBody = nil
-        urlRequest.allHTTPHeaderFields = params.auth.authHeaders()
-          .appending(["Account-Id": params.auth.accountId])
-        // .appending(NetworkUtilities.defaultBackblazeHeaders)
-        return urlRequest
-      }
-      
-      do {
-        let computers = try await urlRequest().fetchResponse([ListComputers.Response.Computer].self)
-        
-        return computers.map {
-          Log4swift[Self.self].info("file: \($0)")
-          return $0
+    public static let liveValue = Self(
+        listBuckets: { params in
+            func urlRequest() throws -> URLRequest {
+                let relativeURL = "b2api/v2/b2_list_buckets"
+                let absoluteURL = params.auth.apiUrl.appendingPathComponent(relativeURL)
+                
+                var urlRequest = URLRequest(url: absoluteURL)
+                // in seconds
+                urlRequest.timeoutInterval = 10.0
+                urlRequest.httpMethod = "POST"
+                urlRequest.httpBody = {
+                    let request = ListBuckets.Request(accountId: params.auth.accountId)
+                    return try? JSONEncoder().encode(request)
+                }()
+                urlRequest.allHTTPHeaderFields = params.auth.authHeaders()
+                    .appending(NetworkUtilities.defaultBackblazeHeaders)
+                return urlRequest
+            }
+            
+            do {
+                return try await urlRequest().fetchResponse(ListBuckets.Response.self).buckets
+            } catch {
+                Log4swift[Self.self].error("error: \(error)")
+                throw error
+            }
+        },
+        listEntriesInDir: { params in
+            func urlRequest() throws -> URLRequest {
+                let relativeURL = params.shouldReturnFolders ? "b2api/v2/b2_list_file_versions" : "b2api/v2/b2_list_file_names"
+                let absoluteURL = params.auth.apiUrl.appendingPathComponent(relativeURL)
+                
+                var urlRequest = URLRequest(url: absoluteURL)
+                // in seconds
+                // long timeout for slow loading trees
+                urlRequest.timeoutInterval = 600.0
+                urlRequest.httpMethod = "POST"
+                urlRequest.httpBody = {
+                    let delimiter = params.shouldReturnFolders ? "/" : nil
+                    let request = ListEntriesInDir.Request(bucketId: params.bucketId, startFileName: params.startFileName, delimiter: delimiter, prefix: params.startFileName)
+                    return try? JSONEncoder().encode(request)
+                }()
+                urlRequest.allHTTPHeaderFields = params.auth.authHeaders()
+                    .appending(NetworkUtilities.defaultBackblazeHeaders)
+                return urlRequest
+            }
+            
+            do {
+                return try await urlRequest().fetchResponse(ListEntriesInDir.Response.self).files
+            } catch {
+                Log4swift[Self.self].error("error: \(error)")
+                throw error
+            }
+        },
+        listComputers: { params in
+            func urlRequest() throws -> URLRequest {
+                let relativeURL = "api2/list_b1_computers"
+                let absoluteURL = params.auth.apiUrl.appendingPathComponent(relativeURL)
+                    .appendingQueryItem(name: "user_id", value: params.auth.accountId)
+                    .appendingQueryItem(name: "bz_device_id", value: params.deviceId)
+                    .appendingQueryItem(name: "cluster_num", value: params.clusterNum)
+                
+                // Some POST requests may also want parameters in the url
+                // and can have a nil body
+                
+                var urlRequest = URLRequest(url: absoluteURL)
+                // in seconds
+                urlRequest.timeoutInterval = 10.0
+                urlRequest.httpMethod = "POST"
+                urlRequest.httpBody = nil
+                urlRequest.allHTTPHeaderFields = params.auth.authHeaders()
+                    .appending(["Account-Id": params.auth.accountId])
+                // .appending(NetworkUtilities.defaultBackblazeHeaders)
+                return urlRequest
+            }
+            
+            do {
+                let computers = try await urlRequest().fetchResponse([ListComputers.Response.Computer].self)
+                
+                return computers.map {
+                    Log4swift[Self.self].info("file: \($0)")
+                    return $0
+                }
+                return []
+            } catch {
+                Log4swift[Self.self].error("error: \(error)")
+                throw error
+            }
+        },
+        authorizeAccount: { params in
+            func urlRequest() throws -> URLRequest {
+                // MARK: - Valid URL? https://api002.backblazeb2.com
+                guard let url = URL(string: "https://api002.backblazeb2.com/b2api/v2/b2_authorize_account"),
+                      let authHeader = params.authHeader()
+                else { throw ApiError(status: 400, code: "400", message: "failed to init authorizeAccount URL / headers") }
+                var urlRequest = URLRequest(url: url)
+                urlRequest.timeoutInterval = 10.0 // in seconds
+                urlRequest.httpMethod = "GET"
+                urlRequest.allHTTPHeaderFields = authHeader.appending(NetworkUtilities.defaultBackblazeHeaders)
+                return urlRequest
+            }
+            
+            do {
+                return try await urlRequest().fetchResponse(AuthorizeAccount.Response.self)
+            } catch {
+                Log4swift[Self.self].error("error: \(error)")
+                throw error
+            }
         }
-        return []
-      } catch {
-        Log4swift[Self.self].error("error: \(error)")
-        throw error
-      }
-    },
-    authorizeAccount: { params in
-      func urlRequest() throws -> URLRequest {
-        // MARK: - Valid URL? https://api002.backblazeb2.com
-        guard let url = URL(string: "https://api002.backblazeb2.com/b2api/v2/b2_authorize_account"),
-              let authHeader = params.authHeader()
-        else { throw ApiError(status: 400, code: "400", message: "failed to init authorizeAccount URL / headers") }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.timeoutInterval = 10.0 // in seconds
-        urlRequest.httpMethod = "GET"
-        urlRequest.allHTTPHeaderFields = authHeader.appending(NetworkUtilities.defaultBackblazeHeaders)
-        return urlRequest
-      }
-      
-      do {
-        return try await urlRequest().fetchResponse(AuthorizeAccount.Response.self)
-      } catch {
-        Log4swift[Self.self].error("error: \(error)")
-        throw error
-      }
-    }
-    
-  )
+        
+    )
 }
 
 extension B2ApiClient: TestDependencyKey {
-  public static let previewValue = Self(
-    listBuckets: { params in
-      return []
-    },
-    listEntriesInDir: { params in
-      return []
-    },
-    listComputers: { params in
-      return []
-    },
-    authorizeAccount: { params in
-        return .init(accountId: "TBD", authorizationToken: "TBD", apiUrl: URL.init(fileURLWithPath: NSTemporaryDirectory()))
-    }
+    public static let previewValue = Self(
+        listBuckets: { params in
+            return []
+        },
+        listEntriesInDir: { params in
+            return []
+        },
+        listComputers: { params in
+            return []
+        },
+        authorizeAccount: { params in
+            return .init(accountId: "TBD", authorizationToken: "TBD", apiUrl: URL.init(fileURLWithPath: NSTemporaryDirectory()))
+        }
+        
+    )
     
-  )
-  
-  public static let testValue = Self(
-    listBuckets: XCTUnimplemented("\(Self.self).listBuckets"),
-    listEntriesInDir: XCTUnimplemented("\(Self.self).listEntriesInDir"),
-    listComputers: XCTUnimplemented("\(Self.self).listComputers"),
-    authorizeAccount: XCTUnimplemented("\(Self.self).authorizeAccount")
-  )
+    public static let testValue = Self(
+        listBuckets: XCTUnimplemented("\(Self.self).listBuckets"),
+        listEntriesInDir: XCTUnimplemented("\(Self.self).listEntriesInDir"),
+        listComputers: XCTUnimplemented("\(Self.self).listComputers"),
+        authorizeAccount: XCTUnimplemented("\(Self.self).authorizeAccount")
+    )
 }
--- B2Api/Tests/B2ApiTests/B2ApiTests.swift
+++ B2Api/Tests/B2ApiTests/B2ApiTests.swift
@@ -6,111 +6,107 @@ import XCTest
 
 @MainActor
 final class B2ApiTests: XCTestCase {
-  /// these will prop expire but cool for testing
-  let auth = Authentication(
-    apiUrl: URL(string: "https://api002.backblazeb2.com")!,
-    accountId: "d365fdf3dfcc",
-    authToken: "5_002-34fa50f0-cd6e-11ed-bf03-cfbea63e1e16_1680010879509_01ab4825_YoCWg7_RTorssR2SLn3wdZk6Z8UzuVdC24="
-  )
-  
-  //      - authToken : "5_002-b86daa80-cf0e-11ed-939b-75e395581e99_1680189777716_01ab53ca_mMXxWp_l1ISYayUWoEdVC-NcP3ZP4Z55po="
-  
-  override func setUp() async throws {
-    Log4swiftConfig.configureLogs(defaultLogFile: nil, lock: "IDDLogLock")
-  }
-  
-  /// given an authentication, copy paste this from the app or postman
-  /// run the b2ApiClient.listBuckets
-  func testListBuckets() async throws {
-    _ = try await withDependencies {
-      // we want to test the live implementation for this test
-      $0.b2ApiClient = .liveValue
-    } operation: {
-      @Dependency(\.b2ApiClient) var b2ApiClient
-      
-      let param = ListBuckets(auth: auth)
-      let buckets = try await b2ApiClient.listBuckets(param)
-        .map { bucket in
-          // feel free to compose more work
-          Log4swift[Self.self].info("bucket: \(bucket)")
-          return bucket
-        }
-      Log4swift[Self.self].info("buckets: \(buckets)")
-      
-      XCTAssertEqual(buckets.count, 2)
-    }
-  }
-  
-  /// given an authentication, copy paste this from the app or postman
-  /// run the b2ApiClient.listBuckets and listEntriesInDir for each of those buckets
-  func testListEntriesInDir() async throws {
-    _ = try await withDependencies {
-      // we want to test the live implementation for this test
-      $0.b2ApiClient = .liveValue
-    } operation: {
-      @Dependency(\.b2ApiClient) var b2ApiClient
-      
-      let param = ListBuckets(auth: auth)
-      let buckets = try await b2ApiClient.listBuckets(param)
-      NSLog("buckets: \(buckets)")
-      
-      let allFolders = try? await buckets.asyncMap { bucket in
-        let param = ListEntriesInDir(auth: auth, bucketId: bucket.bucketId, startFileName: nil, shouldShowFileVersions: false, shouldReturnFolders: true)
-        let dirEntries = try await b2ApiClient.listEntriesInDir(param)
-        return dirEntries
-      }
-      Log4swift[Self.self].info("allFolders: \(allFolders)")
-      
-      XCTAssertEqual(buckets.count, 2)
-    }
-  }
-  
-  /// given an authentication, copy paste this from the app or postman
-  /// run the b2ApiClient.listComputers
-  func testListComputers() async throws {
-    _ = try await withDependencies {
-      // we want to test the live implementation for this test
-      $0.b2ApiClient = .liveValue
-    } operation: {
-      @Dependency(\.b2ApiClient) var b2ApiClient
-      
-      let auth = Authentication(
-        apiUrl: URL(string: "https://ca002.backblaze.com")!,
-        accountId: "d365fdf3dfcc",
-        authToken: "5_002-4042a810-cf10-11ed-80a7-65550f728124_1680190431643_01ab53d5_8Sdy0F_J1FBESluOxyx-zLAjVSj4xZlAEU="
-      )
-      let param = ListComputers(auth: auth, clusterNum: "002", deviceId: "ios_device")
-      let computers = try await b2ApiClient.listComputers(param)
-        .map { computer in
-          Log4swift[Self.self].info("computer: \(computer)")
-          return computer
-        }
-      Log4swift[Self.self].info("computers: \(computers)")
-      
-      XCTAssertEqual(computers.count, 11)
-    }
-  }
-  
-  func testAuthorizeAccount() async throws {
-    _ = try await withDependencies {
-      $0.b2ApiClient = .liveValue
-    } operation: {
-      @Dependency(\.b2ApiClient) var b2ApiClient
-      
-      let param = AuthorizeAccount.Request(
-        applicationKeyId: "0057bc15b3584db0000000001",
-        applicationKey: "K005EueuAu9rB/CATltSWhBVJfuKT5A"
-      )
-      let response = try await b2ApiClient.authorizeAccount(param)
+    private static var auth = Authentication.empty
+    var logInit = false
 
-        Log4swift[Self.self].info("response: \(response)")
-
-      // TODO: how to assert this private data
-//      XCTAssertTrue(response.accountId == "foo")
-//      XCTAssertTrue(response.authorizationToken == "foo")
-//      XCTAssertTrue(response.apiUrl == "foo")
+    override func setUp() async throws {
+        guard !logInit
+        else {
+            // log to console, configure it once
+            logInit = true
+            LoggingSystem.bootstrap { label in
+                ConsoleHandler(label: label)
+            }
+            return
+        }
     }
-  }
+
+    /// given an authentication, copy paste this from the app or postman
+    /// run the b2ApiClient.listBuckets
+    func testListBuckets() async throws {
+        _ = try await withDependencies {
+            // we want to test the live implementation for this test
+            $0.b2ApiClient = .liveValue
+        } operation: {
+            @Dependency(\.b2ApiClient) var b2ApiClient
+
+            let param = ListBuckets(auth: Self.auth)
+            let buckets = try await b2ApiClient.listBuckets(param)
+                .map { bucket in
+                    // feel free to compose more work
+                    Log4swift[Self.self].info("bucket: \(bucket)")
+                    return bucket
+                }
+            Log4swift[Self.self].info("buckets: \(buckets)")
+
+            XCTAssertEqual(buckets.count, 3)
+        }
+    }
+
+    /// given an authentication, copy paste this from the app or postman
+    /// run the b2ApiClient.listBuckets and listEntriesInDir for each of those buckets
+    func testListEntriesInDir() async throws {
+        _ = try await withDependencies {
+            // we want to test the live implementation for this test
+            $0.b2ApiClient = .liveValue
+        } operation: {
+            @Dependency(\.b2ApiClient) var b2ApiClient
+
+            let param = ListBuckets(auth: Self.auth)
+            let buckets = try await b2ApiClient.listBuckets(param)
+            Log4swift[Self.self].info("buckets: \(buckets)")
+
+            let allFolders = try? await buckets.asyncMap { bucket in
+                let param = ListEntriesInDir(auth: Self.auth, bucketId: bucket.bucketId, startFileName: nil, shouldShowFileVersions: false, shouldReturnFolders: true)
+                let dirEntries = try await b2ApiClient.listEntriesInDir(param)
+                return dirEntries
+            }
+            Log4swift[Self.self].info("allFolders: \(allFolders)")
+
+            XCTAssertEqual(buckets.count, 3)
+        }
+    }
+
+    /// given an authentication, copy paste this from the app or postman
+    /// run the b2ApiClient.listComputers
+    func testListComputers() async throws {
+        _ = try await withDependencies {
+            // we want to test the live implementation for this test
+            $0.b2ApiClient = .liveValue
+        } operation: {
+            @Dependency(\.b2ApiClient) var b2ApiClient
+
+            let param = ListComputers(auth: Self.auth, clusterNum: "002", deviceId: "ios_device")
+            let computers = try await b2ApiClient.listComputers(param)
+                .map { computer in
+                    Log4swift[Self.self].info("computer: \(computer)")
+                    return computer
+                }
+            Log4swift[Self.self].info("computers: \(computers)")
+
+            XCTAssertEqual(computers.count, 11)
+        }
+    }
+
+    /**
+     Run this test first and capture the authorization for use on the other tests
+     */
+    func testAuthorizeAccount() async throws {
+        _ = try await withDependencies {
+            $0.b2ApiClient = .liveValue
+        } operation: {
+            @Dependency(\.b2ApiClient) var b2ApiClient
+
+            let param = AuthorizeAccount.Request(
+                applicationKeyId: "0057bc15b3584db0000000001",
+                applicationKey: "K005EueuAu9rB/CATltSWhBVJfuKT5A"
+            )
+            let response = try await b2ApiClient.authorizeAccount(param)
+
+            Log4swift[Self.self].info("response: \(response)")
+            Self.auth = response.authentication
+        }
+    }
 }
 
 /// What we would like is to request this auth, then use it in every request...
